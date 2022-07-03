6e6fbc5814cfc752182030bc5abc11e969a529aa
mfrknsari <mfrknsari@gmail.com>
2022-07-03 16:49:26 +0300

    first-commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'angular13-password';
+
+    password = '';
+    length: number = 0;
+    includeLetters: boolean = false;
+    includeNumbers: boolean = false;
+    includeSymbols: boolean = false;
+
+    modifyLength(value: string) {
+        const parsedValue = parseInt(value);
+
+        if (!isNaN(parsedValue)) {
+            this.length = parsedValue;
+        }
+
+    }
+
+    modifyLetters() {
+        this.includeLetters = !this.includeLetters;
+    }
+
+    modifyNumbers() {
+        this.includeNumbers = !this.includeNumbers;
+    }
+
+    modifySymbols() {
+        this.includeSymbols = !this.includeSymbols;
+    }
+
+    buttonClick() {
+
+        const numbers = '1234567890';
+        const letters = 'abcdefghijklmnoprstuvyz';
+        const symbols = '!@#$%&/()=';
+
+        let generatedPassword = '';
+        let validChars = '';
+
+        if (this.includeLetters) validChars += letters;
+        if (this.includeNumbers) validChars += numbers;
+        if (this.includeSymbols) validChars += symbols;
+
+        for (let i = 0; i < this.length; i++) {
+            const index = Math.floor(Math.random() * validChars.length);
+            generatedPassword += validChars[index];
+        }
+        this.password = generatedPassword;
+    }
+}
